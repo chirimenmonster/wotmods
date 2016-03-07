795bca38142e7422953b9d736d826b76dfde8a79
chirimenmonster <chirimenmonster@users.noreply.github.com>
2016-03-07 18:11:20 +0900

    add new style loader

---
+++ files/uncompyled/wot_folder/scripts/client/gui/mods/mods_spotmessanger.py
@@ -0,0 +1,15 @@
+
+from debug_utils import LOG_CURRENT_EXCEPTION
+from spotmessanger.SpotMessanger_plugin.SpotMessanger import SpotMessanger
+
+def init():
+    '''Mod's main entry point.  Called by WoT's built-in mod loader.'''
+
+    try:
+        SpotMessanger.readConfig()
+        if SpotMessanger.pluginEnable:
+            print '---> Loading SpotMessanger'
+            SpotMessanger.run()
+        
+    except:
+        LOG_CURRENT_EXCEPTION()
