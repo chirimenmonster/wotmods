6657e777731166110a95b183e3dbde7e68b1017e
chirimenmonster <chirimenmonster@users.noreply.github.com>
2016-04-05 10:52:26 +0900

    update version strings

--- src/scripts/client/gui/mods/spotmessanger/version.py
+++ src/scripts/client/gui/mods/spotmessanger/version.py
@@ -1,6 +1,6 @@
 
 class MOD_INFO:
     NAME ='SpotMessanger'
-    VERSION = '0.2.0'
-    VERSION_LONG = VERSION + ' release'
+    VERSION = '0.2.1'
+    VERSION_LONG = VERSION + ' development'
     DEBUG = 'False'
