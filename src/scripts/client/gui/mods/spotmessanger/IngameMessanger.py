82f3a14461a381ae43871fe3cec6d1a02e4dde87
chirimenmonster <chirimenmonster@users.noreply.github.com>
2016-04-17 12:43:32 +0900

    code clean up

--- src/scripts/client/gui/mods/spotmessanger/IngameMessanger.py
+++ src/scripts/client/gui/mods/spotmessanger/IngameMessanger.py
@@ -13,21 +13,15 @@ from chat_shared import CHAT_COMMANDS
 
 from logger import log
 
-CHANNEL_TYPE = {
-    'TEAM': BATTLE_CHANNEL.TEAM,
-    'COMMON': BATTLE_CHANNEL.COMMON,    
-    'SQUAD': BATTLE_CHANNEL.SQUAD
-}
-
-class _FindCriteria(IEntityFindCriteria):
+class _Criteria(IEntityFindCriteria):
 
     def __init__(self, channelSetting):
-        super(_FindCriteria, self).__init__()
+        super(_Criteria, self).__init__()
         self.__channelSetting = channelSetting
 
     def filter(self, channel):
         return channel.getProtoType() is PROTO_TYPE.BW_CHAT2 and channel.getProtoData().settings is self.__channelSetting
-
+        
 class IngameMessanger(object):
     _cooldDown = 0
     _controllers = None
@@ -47,45 +41,37 @@ class IngameMessanger(object):
         self._cooldDown += delay
 
     def doPing(self, cellIdx):
-        # class gui.battle_control.ChatCommandsController.ChatCommandsController
-        # method:　sendCommand(cmdName)
-        # method: sendAttentionToCell(cellIdx)
         controller = g_sessionProvider.getChatCommands()
         self._setCallback(self._commandDelay, partial(controller.sendAttentionToCell, cellIdx))
         return True
 
     def callHelp(self):
-        # class gui.battle_control.ChatCommandsController.ChatCommandsController
-        # method:　sendCommand(cmdName)
-        # method: sendAttentionToCell(cellIdx)
         controller = g_sessionProvider.getChatCommands()
         self._setCallback(self._commandDelay, partial(controller.sendCommand, CHAT_COMMANDS.HELPME.name()))
         return True
 
     def sendText(self, channel, text):
-        controller = self._channelsCtrl.getControllerByCriteria(_FindCriteria(CHANNEL_TYPE[channel]))
+        controller = self._channelsCtrl.getControllerByCriteria(_Criteria(channel))
         log.debug('found controller: {}'.format(controller))
         if not controller:
-            log.debug('not found: BATTLE_CHANNEL.{}'.format(channel))
+            log.debug('not found channel: {}'.format(channel.name))
             return False
-        log.debug('found: BATTLE_CHANNEL.{}'.format(channel))
-        # sendMessage() is method of class TeamChannelController, SquadChannelController
-        # defined in super class messenger.gui.Scaleform.channels._layout._BattleLayout
+        log.debug('found channel: {}'.format(channel.name))
         self._setCallback(self._textDelay, partial(controller.sendMessage, text))
         return True
 
     def sendTeam(self, text):
         log.debug('send to BATTLE_CHANNEL.TEAM')
-        return self.sendText('TEAM', text)
+        return self.sendText(BATTLE_CHANNEL.TEAM, text)
 
     def sendSquad(self, text):
         log.debug('send to BATTLE_CHANNEL.SQUAD')
-        return self.sendText('SQUAD', text)
+        return self.sendText(BATTLE_CHANNEL.SQUAD, text)
 
-    def getKeys(self):
-        keys = []
-        for channel in CHANNEL_TYPE.keys():
-            if self._channelsCtrl.getControllerByCriteria(_FindCriteria(CHANNEL_TYPE[channel])):
-                keys.append(channel)
-        return keys
+    def getChannelLabels(self):
+        labels = []
+        for channel in BATTLE_CHANNEL.ALL:
+            if self._channelsCtrl.getControllerByCriteria(_Criteria(channel)):
+                labels.append(channel.name)
+        return labels
 
