49ec870834cfd5ab39367aa756c17fccabac885b
chirimenmonster <chirimenmonster@users.noreply.github.com>
2016-05-24 21:47:22 +0900

    change version to 0.3.0

--- src/scripts/client/gui/mods/spotmessanger/version.py
+++ src/scripts/client/gui/mods/spotmessanger/version.py
@@ -1,12 +1,6 @@
 
 class MOD_INFO:
     NAME ='SpotMessanger'
-<<<<<<< HEAD
-    VERSION = '0.2.0'
+    VERSION = '0.3.0'
     VERSION_LONG = VERSION + ' release'
     DEBUG = 'False'
-=======
-    VERSION = '0.3-dev'
-    VERSION_LONG = VERSION + ' development version'
-    DEBUG = 'True'
->>>>>>> develop
