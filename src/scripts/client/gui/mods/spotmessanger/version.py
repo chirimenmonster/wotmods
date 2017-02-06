c14249c6a327efd35845452d3ffae7a7d7cefee5
chirimenmonster <chirimenmonster@users.noreply.github.com>
2017-02-06 21:23:51 +0900

    change build script

--- src/scripts/client/gui/mods/spotmessanger/version.py
+++ src/scripts/client/gui/mods/spotmessanger/version.py
@@ -1,6 +1,6 @@
 
 class MOD_INFO:
     NAME ='SpotMessanger'
-    VERSION = '0.7.0'
-    VERSION_LONG = VERSION + ' release'
+    VERSION = '0.8.0-dev'
+    VERSION_LONG = VERSION + ' development'
     DEBUG = 'False'
