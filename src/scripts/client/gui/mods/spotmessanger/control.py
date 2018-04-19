9a4b384ec277754dd0533923dca2d80f10314a35
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-04-19 22:55:15 +0900

    change log message

--- src/scripts/client/gui/mods/spotmessanger/control.py
+++ src/scripts/client/gui/mods/spotmessanger/control.py
@@ -45,7 +45,7 @@ class SpotMessanger(object):
         if self._isObserver:
             log.info('player avatar is observer, nothing to do')
             return
-        log.info('current battle type: {} [{}({}) = "{}"], "{}" ({})'.format(
+        log.info('current battle type: "{}" [{}({}) = "{}"], "{}" ({})'.format(
             guiType.battleType, guiType.attrLabel, guiType.id, guiType.name,
             arenaType.name, arenaType.geometryName))
         log.info('current vehicle class: {} [{}] ({})'.format(vehicle.classAbbr, vehicle.className, vehicle.name))
