9f7e3bed50b655899508e5a45da6f195b4057498
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-05-16 17:47:15 +0900

    change release script.  split config.ini for wotmod and zip

--- package.py
+++ package.py
@@ -1,14 +1,12 @@
 from zipfile import ZIP_DEFLATED
 from tools import package
 
-CONFIG = 'config.ini'
-
 def main():
-    control = package.Control()
-
+    control = package.Control(config='config.ini')
     file = control.makePackage()
     print 'create package: {}'.format(file)
-    
+
+    control = package.Control(config=['config.ini', 'release.ini'], hasBuildDir=True)
     file = control.makePackage(package.SECTION_RELEASE, compression=ZIP_DEFLATED)
     print 'create package: {}'.format(file)
 
